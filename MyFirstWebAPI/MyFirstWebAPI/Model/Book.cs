ba084f09a79fbb4d27ef8248bde8893659673654
sanghami.d <sanghami.d@gavstech.com>
2023-09-21 14:03:11 +0530

    Include book and bookController

---
+++ MyFirstWebAPI/MyFirstWebAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstWebAPI.Model
+{
+    public class Book
+    {
+        public int BookID { set; get; }
+        public string Title { set; get; }
+        public decimal Cost { set; get; }
+        public string AuthorName { set; get; }
+    }
+}
